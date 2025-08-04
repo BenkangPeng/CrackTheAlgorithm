5950bc928d52237f9847796a2182e324a252dca7
benkangpeng <benkangpeng@163.com>
2025-08-04 23:17:36 +0800

    update class046

--- src-cpp/class046/Code07_find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ src-cpp/class046/Code07_find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -3,9 +3,11 @@
 /// 每遇到一个元音字母,翻转相应位
 /// 向前找prefix相同(奇偶状态相同)的位置(同奇同偶)j,
 /// 那么[j,i)的子数组中元音均为偶数
+#include <climits>
 #include <string>
 #include <unordered_map>
-class Solution {
+#include <vector>
+class Solution0 {
 public:
   int findTheLongestSubstring(const std::string &s) {
     /// 'a'为最低位
@@ -32,6 +34,48 @@ public:
         map[prefix] = i + 1;
       }
     }
+    return ans;
+  }
+};
+
+/// 优化常数时间
+class Solution {
+public:
+  int findTheLongestSubstring(const std::string &s) {
+
+    int n = s.size();
+    int prefix = 31;
+    std::vector<int> map(32, INT_MAX);
+    map[31] = 0;
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      switch (s[i]) {
+      case 'a':
+        prefix ^= 1;
+        break;
+      case 'e':
+        prefix ^= 2;
+        break;
+      case 'i':
+        prefix ^= 4;
+        break;
+      case 'o':
+        prefix ^= 8;
+        break;
+      case 'u':
+        prefix ^= 16;
+        break;
+      default:
+        break;
+      }
+      if (map[prefix] != INT_MAX) {
+        int len = i + 1 - map[prefix];
+        ans = ans > len ? ans : len;
+      } else {
+        map[prefix] = i + 1;
+      }
+    }
+
     return ans;
   }
 };
